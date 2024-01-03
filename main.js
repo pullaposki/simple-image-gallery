5cee4e40548008a7627ef834bee7258540399359
pullaposki <pullaposki@windowslive.com>
2024-01-03 16:53:59 +0200

    Refactor main.js for clarity and specificity
    The main.js file has been refactored to enhance readability and specificity. Code for creating new images and setting image opacity is now enclosed in individual functions. The variable name 'btn' has been replaced with 'themeToggleButton' to improve clarity. The boolean variable 'isDark' has been renamed to 'isThemeDark' for better context.

--- main.js
+++ main.js
@@ -1,42 +1,54 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-const btn = document.querySelector('button');
+const themeToggleButton = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 const IMAGES = [
-    { filePath: "./images/pic1.jpg", altText: "pic1.jpg" },
-    { filePath: "./images/pic2.jpg", altText: "pic2.jpg" },
-    { filePath: "./images/pic3.jpg", altText: "pic3.jpg" },
-    { filePath: "./images/pic4.jpg", altText: "pic4.jpg" },
-    { filePath: "./images/pic5.jpg", altText: "pic5.jpg" },
-]
+    {filePath: "./images/pic1.jpg", altText: "pic1.jpg"},
+    {filePath: "./images/pic2.jpg", altText: "pic2.jpg"},
+    {filePath: "./images/pic3.jpg", altText: "pic3.jpg"},
+    {filePath: "./images/pic4.jpg", altText: "pic4.jpg"},
+    {filePath: "./images/pic5.jpg", altText: "pic5.jpg"},
+];
 
-for (let i = 0; i<IMAGES.length; i++){
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', IMAGES[i].filePath);
-    newImage.setAttribute('alt', IMAGES[i].altText);
-    
-    newImage.addEventListener("click", ()=>{
-        displayedImage.src = IMAGES[i].filePath;
-    })
+const DEFAULT_OPACITY = "1.0";
+const HOVER_OPACITY = "0.7";
 
-    newImage.addEventListener("mouseover", ()=>{
-        newImage.style.opacity = "0.7";
-    })
-    newImage.addEventListener("mouseout", ()=>{
-        newImage.style.opacity = "1.0";
-    })
-    
+for (let i = 0; i < IMAGES.length; i++) {
+    const newImage = createNewImage(IMAGES[i]);
     thumbBar.appendChild(newImage);
 }
 
-let isDark= false;
-btn.addEventListener("click", ()=>{
-    if(isDark){
+let isThemeDark = false;
+
+themeToggleButton.addEventListener("click", changeTheme);
+
+function createNewImage(image) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image.filePath);
+    newImage.setAttribute('alt', image.altText);
+
+    newImage.addEventListener("click", () => setImage(image.filePath));
+    newImage.addEventListener("mouseover", () => setImageOpacity(newImage, HOVER_OPACITY));
+    newImage.addEventListener("mouseout", () => setImageOpacity(newImage, DEFAULT_OPACITY));
+
+    return newImage;
+}
+
+function setImage(filePath) {
+    displayedImage.src = filePath;
+}
+
+function setImageOpacity(image, opacity) {
+    image.style.opacity = opacity;
+}
+
+function changeTheme() {
+    if (isThemeDark) {
         overlay.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
-        isDark=false;
-    }else{
+        isThemeDark = false;
+    } else {
         overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-        isDark=true;
+        isThemeDark = true;
     }
-})
+}
